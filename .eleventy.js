dfcad17f25dcae5e414900a66b950c07bd89c157
MS <65461557+mshafat@users.noreply.github.com>
2026-04-30 20:17:58 +0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,12 @@
-
+module.exports = function(eleventyConfig) {
+  // এটি অ্যাডমিন ফোল্ডারটিকে আউটপুট ফোল্ডারে কপি করে দেয়
+  eleventyConfig.addPassthroughCopy("admin");
+  
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
